0c12ddb7962e10a7e17fec1254fbb46566410445
ewaszy <ewa.a.szyszkowska@gmail.com>
2021-09-25 21:44:45 -0500

    Created Mapping Lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -3,33 +3,44 @@ console.log("working");
 
 // Create the map object with a center and zoom level. (2 Ways to do this)
 // First Way
-// let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 // Second Way
 // Useful when we need to add multiple tile layers, or a background image of our map(s)
-let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  });
+//let map = L.map("mapid", {
+    //center: [
+      //40.7, -94.5
+    //],
+    //zoom: 4
+  //});
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+  // San Francisco - SFO
+  [37.6213, -122.3790],
+  // Denver - DEN (Bonus)
+  [39.7392, -104.9903],
+  // Austin - AUS
+  [30.2672, -97.7431],
+  // Toronto - YYZ
+  [43.6532, -79.3832],
+  // New York - JFK
+  [40.6413, -73.7781]
+];
+
+// Create a polyline using the line coordinates and make the line black.
+L.polyline(line, {
+  color: "blue",
+  weight: '4',
+  opacity: '0.5',
+  dashArray: '8, 8', 
+  dashOffset: '0'
+}).addTo(map);
 
 // Get data from cities.js
 let cityData = cities;
 
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-  console.log(city)
-  L.circleMarker(city.location, {
-    radius: city.population/100000,
-    color: "orange",
-    weight: 4
-  })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-.addTo(map);
-});
-
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
